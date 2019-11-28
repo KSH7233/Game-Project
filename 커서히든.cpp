3724217cf5f0b7b1fc23030c42cb193854e76047
parkjimin-tech <55951219+parkjimin-tech@users.noreply.github.com>
2019-11-28 10:47:21 +0900

    Create 커서히든.cpp

---
+++ 커서히든.cpp
@@ -0,0 +1,13 @@
+
+#include<Windows.h>
+void CursorView(char show)
+{
+	HANDLE hConsole;
+	CONSOLE_CURSOR_INFO ConsoleCursor;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	ConsoleCursor.bVisible = show;
+	ConsoleCursor.dwSize = 1;
+
+	SetConsoleCursorInfo(hConsole, &ConsoleCursor);
+}
